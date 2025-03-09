2425c7cf78a2e9c8bba508ec82925ae229beaf26
amujtaba527 <amujtaba527@gmail.com>
2025-03-09 12:36:36 +0500

    Login changes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -43,7 +43,7 @@ export default function LoginPage() {
               Email
             </label>
             <input
-              type="email"
+              type="text"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
