119525e7aa304e5daa5a703452b03cb990e51c5f
amujtaba527 <amujtaba527@gmail.com>
2025-03-09 13:05:40 +0500

    db

--- init-db.sql
+++ init-db.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users (
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,  -- Store hashed passwords
-    role VARCHAR(50) CHECK (role IN ('admin', 'employee')) NOT NULL DEFAULT 'employee',
+    role VARCHAR(50) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -25,29 +25,14 @@ CREATE TABLE IF NOT EXISTS complaints (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    building VARCHAR(100) CHECK (building IN ('Building A', 'Building B', 'Building C')) NOT NULL,
-    floor VARCHAR(50) CHECK (floor IN ('Basement', 'Ground', '1st', '2nd')) NOT NULL,
+    building VARCHAR(100)  NOT NULL,
+    floor VARCHAR(50)  NOT NULL,
     area_id INT NOT NULL,
     complaint_type_id INT NOT NULL,
     details TEXT NOT NULL,
-    status VARCHAR(50) CHECK (status IN ('In-Progress', 'Resolved')) DEFAULT 'In-Progress',
-    resolution_date TIMESTAMP NULL,  -- Can only be updated by admin
+    status VARCHAR(50),
+    resolution_date TIMESTAMP NULL,  
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (area_id) REFERENCES areas(id) ON DELETE CASCADE,
     FOREIGN KEY (complaint_type_id) REFERENCES complaint_types(id) ON DELETE CASCADE
 );
-
--- Insert Some Default Complaint Types
--- INSERT INTO complaint_types (type_name) VALUES
--- ('Electrical Issue'),
--- ('Plumbing Issue'),
--- ('Internet Issue'),
--- ('Furniture Repair');
-
--- Insert Some Default Areas
--- INSERT INTO areas (area_name) VALUES
--- ('Library'),
--- ('Cafeteria'),
--- ('Office'),
--- ('Classroom');
-
